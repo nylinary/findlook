b1c25b9daa2d9cbe82206cb5ad7bf792cf63649c
Nikita Sysoev <njt55cs@gmail.com>
2022-01-12 21:06:49 +0300

    Added form validation and some js for it

--- findlook_project/main/forms.py
+++ findlook_project/main/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.core import validators
 
+
+def check_if_gmail(value):
+    if value.split('@')[1] != 'gmail.com':
+        raise forms.ValidationError("Only google mail.")
+
 class SuggestionForm(forms.Form):
     name = forms.CharField(required=False)
-    email = forms.EmailField()
+    email = forms.EmailField(validators=[check_if_gmail])
     text = forms.CharField(widget=forms.Textarea(
             attrs={'placeholder': 'Ваши предложения..'}))
     botcatcher = forms.CharField(required=False,
--- findlook_project/main/views.py
+++ findlook_project/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from main.models import Topic, Webpage, AccessRecord
 from . import forms
+import json
 # Create your views here.
 
 
@@ -20,9 +21,15 @@ def main_index(request):
 
 def send_suggestion_page(request):
     form = forms.SuggestionForm()
+    validation_dict = {'success': None}
     if request.POST:
         form = forms.SuggestionForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data['name'])
+            validation_dict['success'] = True
+            return HttpResponse(json.dumps(validation_dict))
+        else:
+            validation_dict['success'] = False
+            return HttpResponse(json.dumps(validation_dict))
+
     return render(request, 'main/suggestion.html', {'form': form})
